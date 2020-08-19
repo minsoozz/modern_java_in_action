92c2dc45df5e7db3e16dce152e7e70f68ee29cd4
minsoozz <minsoo1604@naver.com>
2020-08-20 00:56:16 +0900

    Chapter14

---
+++ first/src/first/Test.java
@@ -0,0 +1,11 @@
+package first;
+
+import second.User;
+
+public class Test {
+
+  public static void main(String[] args) {
+    User minsoo = new User();
+    System.out.println("minsoo = " + minsoo.getName());
+  }
+}
---
+++ second/src/second/User.java
@@ -0,0 +1,22 @@
+package second;
+
+public class User {
+
+  private String name = "minsoo";
+
+  public User() {
+
+  }
+
+  public User(String name) {
+    this.name = name;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+}
